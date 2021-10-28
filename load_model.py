76e26cfae426c4ac2ce4f0a9d8ac4e1765c65b2d
joshmiao <myk0210@163.com>
2021-10-28 21:07:10 +0800

    load tensorflow captcha training model

--- load_model.py
+++ load_model.py
@@ -14,7 +14,7 @@ data_dir = Path("./captcha/")
 # Get list of all the images
 images = sorted(list(map(str, list(data_dir.glob("*.png")))))
 labels = [img.split(os.path.sep)[-1].split(".png")[0] for img in images]
-characters = set(char for label in labels for char in label)
+characters = list(set(char for label in labels for char in label))
 
 print("Number of images found: ", len(images))
 print("Number of labels found: ", len(labels))
