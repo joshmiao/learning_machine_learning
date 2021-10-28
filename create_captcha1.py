964f2295a9233356b4f3d38b0e007bb40096801c
joshmiao <myk0210@163.com>
2021-10-28 19:41:05 +0800

    making captcha training image (substitute)

---
+++ create_captcha1.py
@@ -0,0 +1,53 @@
+import random
+from io import BytesIO
+
+from PIL import Image, ImageDraw, ImageFont
+
+
+def get_random_color():
+    return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+
+def get_valid_code_img(request):
+    img = Image.new('RGB', (260, 50), color=get_random_color())
+    draw = ImageDraw.Draw(img)
+    kumo_font = ImageFont.truetype('./msyh.ttc', size=28)
+
+    valid_code_str = ''
+    for i in range(5):
+        random_num = str(random.randint(0, 9))
+        random_low_alpha = chr(random.randint(97, 122))
+        random_high_alpha = chr(random.randint(65, 90))
+        random_char = random.choice([random_num, random_low_alpha, random_high_alpha])
+        draw.text((i * 50 + 20, 5), random_char, get_random_color(), font=kumo_font)
+
+        # 保存验证码字符串
+        valid_code_str += random_char
+
+    # 噪点噪线
+    width = 260
+    height = 34
+    for i in range(5):
+        x1 = random.randint(0, width)
+        x2 = random.randint(0, width)
+        y1 = random.randint(0, height)
+        y2 = random.randint(0, height)
+        draw.line((x1, y1, x2, y2), fill=get_random_color())
+
+    for i in range(5):
+        draw.point([random.randint(0, width), random.randint(0, height)], fill=get_random_color())
+        x = random.randint(0, width)
+        y = random.randint(0, height)
+        draw.arc((x, y, x + 4, y + 4), 0, 90, fill=get_random_color())
+
+    # 保存验证码字符串到该用户的session
+    # request.session['valid_code_str'] = valid_code_str
+
+    f = BytesIO()  # 用完之后，BytesIO会自动清掉
+    img.save(f, 'png')
+    img.show()
+    data = f.getvalue()
+
+    return data
+
+get_valid_code_img("??")
